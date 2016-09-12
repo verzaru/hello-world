6863becee0a2fce378151cc455498cf4e9fe5192
Verzaru Metallium <gravypunk@hotmail.com>
2016-09-12 15:33:46 +0700

    Add the second Hello World as php

---
+++ hello.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello World!</title>
+</head>
+<body>
+
+<h1>Hello World by PHP</h1>
+<p><?php echo "This is very minimal \"hello world\" HTML document."?></p>
+
+</body>
+</html>
